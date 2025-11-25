b802a03aa9f3e8c384d01e9619f1e7d5930286e9
samarth3301 <samarplayz1337@gmail.com>
2025-11-25 09:39:48 +0530

    Refactor moduleAlias.ts: standardize quotes and formatting

--- src/config/moduleAlias.ts
+++ src/config/moduleAlias.ts
@@ -1,8 +1,8 @@
-import moduleAlias from "module-alias";
-import path from "path";
+import moduleAlias from 'module-alias';
+import path from 'path';
 
 
-const basePath = process.env.NODE_ENV === "production" ? "dist" : "src";
+const basePath = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
 moduleAlias.addAliases({
-	"@": path.join(process.cwd(), basePath),
+  '@': path.join(process.cwd(), basePath),
 });
